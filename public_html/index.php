11ec5f1fc25d143687a23b86b5ebfa0b9045a569
Kai Garrott <garrottkai@gmail.com>
2016-05-08 16:56:09 -0600

    Add stylesheet, bootstrap

--- public_html/index.php
+++ public_html/index.php
@@ -4,8 +4,46 @@
 		<meta charset="UTF-8" />
 		<meta name="author" content="Kai Garrott" />
 
+		<!-- sets IE rendering to IE-EDGE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+		<!-- sets viewport width to device width, scaling 1:1 -->
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- Compiled and minified Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/styles.css" type="text/css">
+
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js"   integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="   crossorigin="anonymous"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+		<!-- Custom JavaScript/jQuery -->
+
+		<title>Whoaa, more bootstrap(s) than Ayn Rand!</title>
 	</head>
-	<body>
-		
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<div class="container">
+					
+				</div>
+			</header>
+		</div>
+
 	</body>
 </html>
